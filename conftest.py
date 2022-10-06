a5e8276e0a5f4872645c4f5bd75853ecf2477119
id301 <mam.nik.al@gmail.com>
2022-10-07 00:34:36 +0300

    file replacement via ftp added

--- conftest.py
+++ conftest.py
@@ -4,20 +4,32 @@ from fixture.application import Application
 import pytest
 import json
 import os.path
+import ftputil
 
 fixture = None
 target = None
 
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))['web']
-    webadmin_config = load_config(request.config.getoption("--target"))['webadmin']
+    web_config = config['web']
+    webadmin_config = config['webadmin']
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["baseUrl"])
     fixture.session.login(username=webadmin_config['username'], password=webadmin_config['password'])
     return fixture
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    def fin():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
@@ -38,3 +50,18 @@ def load_config(file):
         with open(config_file) as f:
             target = json.load(f)
     return target
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
--- test/test_login.py
+++ test/test_login.py
@@ -1,5 +1,5 @@
 __author__ = 'id301'
 
 def test_login(app):
-    app.session.login("administrator", "root")
+    app.session.login("administrator", "root") #for correct use comment login in conftest.py
     assert app.session.is_logged_in_as("administrator")
