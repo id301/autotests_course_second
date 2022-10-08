1996e059f6eb9b325a19ff733968872303653089
id301 <mam.nik.al@gmail.com>
2022-10-08 18:02:26 +0300

    added test_signup_new_account.py

--- fixture/application.py
+++ fixture/application.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
 from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 class Application:
 
@@ -20,6 +22,8 @@ class Application:
         self.project = ProjectHelper(self)
         self.session = SessionHelper(self)
         self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
         self.config = config
         self.base_url = config['web']['baseUrl']
 
