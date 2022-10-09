5234ed43a87ff5d97e950b2e5e0562d4f721ecf4
id301 <mam.nik.al@gmail.com>
2022-10-09 20:29:27 +0300

    some fixes in soap.py

--- fixture/soap.py
+++ fixture/soap.py
@@ -9,17 +9,17 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.config['web']['baseUrl'] + "api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
         except WebFault:
             return False
 
-    def get_projects_list(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+    def get_projects_list(self):
+        client = Client(self.app.config['web']['baseUrl'] + "api/soap/mantisconnect.php?wsdl")
         try:
-            lst = client.service.mc_projects_get_user_accessible(username, password)
+            lst = client.service.mc_projects_get_user_accessible(self.app.config['webadmin']['username'], self.app.config['webadmin']['password'])
             return lst
         except WebFault:
             print("Error occured")
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -4,11 +4,11 @@ from model.project import Project
 from generators.simple_generator import testdata
 
 def test_add_project(app):
-    old_projects = app.soap.get_projects_list(app.config['webadmin']['username'], app.config['webadmin']['password'])
+    old_projects = app.soap.get_projects_list()
     old_projects = app.project.convert_ProjectData_list_to_Project_list(old_projects)
     project = testdata
     app.project.add_project(project)
-    new_projects = app.soap.get_projects_list(app.config['webadmin']['username'], app.config['webadmin']['password'])
+    new_projects = app.soap.get_projects_list()
     new_projects = app.project.convert_ProjectData_list_to_Project_list(new_projects)
     old_projects.append(project)
     assert sorted(old_projects, key=Project.get_name) == sorted(new_projects, key=Project.get_name)
